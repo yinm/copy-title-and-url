ef47f061d7f40010ee9bd54032a3f6d6a2f1c2e2
yinm <yinm.cminor9@gmail.com>
2018-03-11 22:02:53 +0900

    Add select and copy to result

--- popup.js
+++ popup.js
@@ -1,6 +1,10 @@
 'use strict'
 
 chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
-  const tab = tabs[0];
-  document.getElementById('result').value = `[${tab.title}](${tab.url})`
+  const tab = tabs[0]
+  const result = document.getElementById('result')
+
+  result.value = `[${tab.title}](${tab.url})`
+  result.select()
+  document.execCommand('copy')
 });
